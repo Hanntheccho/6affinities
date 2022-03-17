d3df1243602047b6f50e42249e3c6ddc0478bb59
Hanntheccho <jmgp0411@gmail.com>
2022-03-17 23:28:38 +0100

    Third commit

--- questions.js
+++ questions.js
@@ -1,38 +1,274 @@
 questions = [
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "question": "Humanity is inherently flawed in at least one regard.",
         "effect": {
             "econ": 10,
-            "dipl": 0,
-            "govt": -5,
-            "scty": 0
+            "dipl": -5,
+            "govt": 0
         }
     },
     {
-        "question": "It is necessary for the government to intervene in the economy to protect consumers.",
-        "effect": {
-            "econ": 10,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 0
-        }
-    },
-    {
-        "question": "The freer the markets, the freer the people.",
+        "question": "Today's world is inferior to at least one time period that came before.",
         "effect": {
             "econ": -10,
             "dipl": 0,
-            "govt": 0,
-            "scty": 0
+            "govt": 0
         }
     },
     {
-        "question": "It is better to maintain a balanced budget than to ensure welfare for all citizens.",
+        "question": "History as a subject is as useful to modern society as the hard sciences.",
         "effect": {
             "econ": -10,
             "dipl": 0,
-            "govt": 0,
-            "scty": 0
+            "govt": 5
+        }
+    },
+    {
+        "question": "Maintaining cultural values is important, even if those values interfere with a people's ability to survive.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 5
+        }
+    },
+    {
+        "question": "Earth is inherently more important to humanity than any other planet.",
+        "effect": {
+            "econ": -10,
+            "dipl": 5,
+            "govt": 5
+        }
+    },
+    {
+        "question": "Achieving fully self-aware artificial intelligence is a good thing.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": -10
+        }
+    },
+    {
+        "question": "A self-aware artificial intelligence would make for a more efficient and ethical governor than most humans.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": -10
+        }
+    },
+    {
+        "question": "Ethics are more akin to guidelines than concrete rules.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": -5
+        }
+    },
+    {
+        "question": "Humanity is inherently special.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 5
+        }
+    },
+    {
+        "question": "If possible, I would accept bodily augmentations such as mechanical limbs or brain implants.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": -5
+        }
+    },
+    {
+        "question": "Body augmentations are justified and acceptable when applied to support the life of a person due to their unique conditions.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": -5
+        }
+    },
+    {
+        "question": "Body augmentations diminish our humanity.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 5
+        }
+    },
+    {
+        "question": "Natural disasters caused by humans are errors of the human condition.",
+        "effect": {
+            "econ": 5,
+            "dipl": 5,
+            "govt": 0
+        }
+    },
+    {
+        "question": "All art is meaningless or its meaning is only artificially defined by humans.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": -10
+        }
+    },
+    {
+        "question": "It is acceptable to genetically or mechanically modify other species.",
+        "effect": {
+            "econ": 10,
+            "dipl": -5,
+            "govt": 0
+        }
+    },
+    {
+        "question": "A self-aware artificial intelligence has a soul on par with that of a human.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 5
+        }
+    },
+    {
+        "question": "Under certain circumstances, it is acceptable to compromise on one's beliefs.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": -5
+        }
+    },
+    {
+        "question": "The current human form is the ultimate form that a living being can have, and any modification deviates from that perfection.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 5
+        }
+    },
+    {
+        "question": "The needs of human settlers outweigh the importance of maintaining an environment's biodiversity.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0
+        }
+    },
+    {
+        "question": "The well-being of the Earth should be prioritized over space colonization.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0
+        }
+    },
+    {
+        "question": "If an alien species is discovered, it should be respected and preserved regardless of its intelligence or attitude towards humanity.",
+        "effect": {
+            "econ": 5,
+            "dipl": 5,
+            "govt": 0
+        }
+    },
+    {
+        "question": "Industrial pollution is an acceptable sacrifice for the sustainability of the modern world.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0
+        }
+    },
+    {
+        "question": "An agrarian life is superior to an urban one.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 5
+        }
+    },
+    {
+        "question": "If artificial intelligence is to be developed, then its self-awareness should be curtailed.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 5
+        }
+    },
+    {
+        "question": "I try to recycle whenever possible.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0
+        }
+    },
+    {
+        "question": "Government regulations to protect the environment are justified.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0
+        }
+    },
+    {
+        "question": "Climate change is the greatest threat to the well-being of modern society.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0
+        }
+    },
+    {
+        "question": "It is possible to replicate a human mind and soul via machines.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10
+        }
+    },
+    {
+        "question": "Materialism is a threat to the physical and spiritual well-being of humanity.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10
+        }
+    },
+    {
+        "question": "It is acceptable to curtail scientific progress so that cultural understanding and appreciation of certain technologies can catch up.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 10
+        }
+    },
+    {
+        "question": "If there is a meaning to life, it is to live a moral life.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 10
+        }
+    },
+    {
+        "question": "Through sufficient technological advancements, it is possible for humanity to attain omnipotence.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10
+        }
+    },
+    {
+        "question": "The physical body is but a vessel for the human soul.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10
+        }
+    },
+    {
+        "question": "Artificial intelligence is unethical and its creation should always be avoided.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10
         }
     }
 ];
