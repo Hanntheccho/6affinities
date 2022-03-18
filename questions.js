a5df99f1a3ffaa8ced37f975d59cb864474d153f
Hanntheccho <76615612+Hanntheccho@users.noreply.github.com>
2022-03-18 09:36:16 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,15 +2,15 @@ questions = [
     {
         "question": "Humanity is inherently flawed in at least one regard.",
         "effect": {
-            "econ": 20,
+            "econ": -20,
             "dipl": -5,
-            "govt": 0
+            "govt": 5
         }
     },
     {
         "question": "Today's world is inferior to at least one time period that came before.",
         "effect": {
-            "econ": -20,
+            "econ": 20,
             "dipl": 0,
             "govt": 0
         }
@@ -18,61 +18,13 @@ questions = [
     {
         "question": "History as a subject is as useful to modern society as the hard sciences.",
         "effect": {
-            "econ": -20,
-            "dipl": 0,
-            "govt": 5
-        }
-    },
-    {
-        "question": "Maintaining cultural values is important, even if those values interfere with a people's ability to survive.",
-        "effect": {
-            "econ": -20,
-            "dipl": 0,
-            "govt": 5
-        }
-    },
-    {
-        "question": "Earth is inherently more important to humanity than any other planet.",
-        "effect": {
-            "econ": -20,
-            "dipl": 5,
-            "govt": 5
-        }
-    },
-    {
-        "question": "Achieving fully self-aware artificial intelligence is a good thing.",
-        "effect": {
-            "econ": 5,
-            "dipl": 0,
-            "govt": -20
-        }
-    },
-    {
-        "question": "A self-aware artificial intelligence would make for a more efficient and ethical governor than most humans.",
-        "effect": {
-            "econ": 5,
-            "dipl": 0,
-            "govt": -20
-        }
-    },
-    {
-        "question": "Ethics are more akin to guidelines than concrete rules.",
-        "effect": {
-            "econ": 5,
+            "econ": 20,
             "dipl": 0,
             "govt": -5
         }
     },
     {
-        "question": "Humanity is inherently special.",
-        "effect": {
-            "econ": -20,
-            "dipl": 0,
-            "govt": 5
-        }
-    },
-    {
-        "question": "If possible, I would accept bodily augmentations such as mechanical limbs or brain implants.",
+        "question": "Maintaining cultural values is important, even if those values interfere with a people's ability to survive.",
         "effect": {
             "econ": 20,
             "dipl": 0,
@@ -80,11 +32,60 @@ questions = [
         }
     },
     {
+        "question": "Earth is inherently more important to humanity than any other planet.",
+        "effect": {
+            "econ": 20,
+            "dipl": -10,
+            "govt": -5
+        }
+    },
+    {
+        "question": "Achieving fully self-aware artificial intelligence is a good thing.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 20
+        }
+    },
+    {
+        "question": "A self-aware artificial intelligence would make for a more efficient and ethical governor than most humans.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 20
+        }
+    },
+    {
+        "question": "Ethics are more akin to guidelines than concrete rules.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 10
+        }
+    },
+    {
+        "question": "Humanity is inherently special.",
+        "effect": {
+            "econ": 20,
+            "dipl": 0,
+            "govt": -10
+
+        }
+    },
+    {
+        "question": "If possible, I would accept bodily augmentations such as mechanical limbs or brain implants.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 5
+        }
+    },
+    {
         "question": "Body augmentations are justified and acceptable when applied to support the life of a person due to their unique conditions.",
         "effect": {
-            "econ": 5,
+            "econ": -5,
             "dipl": 0,
-            "govt": -5
+            "govt": 10
         }
     },
     {
@@ -92,123 +93,35 @@ questions = [
         "effect": {
             "econ": 20,
             "dipl": 0,
-            "govt": 5
+            "govt": -5
         }
     },
     {
         "question": "Disasters caused by humans are errors of the human condition.",
         "effect": {
-            "econ": 5,
-            "dipl": 5,
+            "econ": -20,
+            "dipl": -10,
             "govt": 0
         }
     },
     {
         "question": "All art is meaningless or its meaning is only artificially defined by humans.",
         "effect": {
-            "econ": 5,
+            "econ": -10,
             "dipl": 0,
-            "govt": -20
+            "govt": 20
         }
     },
     {
         "question": "It is acceptable to genetically or mechanically modify other species.",
         "effect": {
-            "econ": 20,
-            "dipl": -5,
+            "econ": -5,
+            "dipl": 20,
             "govt": 0
         }
     },
     {
         "question": "A self-aware artificial intelligence has a soul on par with that of a human.",
-        "effect": {
-            "econ": 5,
-            "dipl": 0,
-            "govt": 5
-        }
-    },
-    {
-        "question": "Under certain circumstances, it is acceptable to compromise on one's beliefs.",
-        "effect": {
-            "econ": 5,
-            "dipl": 0,
-            "govt": -5
-        }
-    },
-    {
-        "question": "The current human form is the ultimate form that a living being can have, and any modification deviates from that perfection.",
-        "effect": {
-            "econ": -20,
-            "dipl": 0,
-            "govt": 5
-        }
-    },
-    {
-        "question": "The needs of human settlers outweigh the importance of maintaining an environment's biodiversity.",
-        "effect": {
-            "econ": 0,
-            "dipl": -20,
-            "govt": 0
-        }
-    },
-    {
-        "question": "The well-being of the Earth should be prioritized over space colonization.",
-        "effect": {
-            "econ": 0,
-            "dipl": 20,
-            "govt": 0
-        }
-    },
-    {
-        "question": "If an alien species is discovered, it should be respected and preserved regardless of its intelligence or attitude towards humanity.",
-        "effect": {
-            "econ": 5,
-            "dipl": 5,
-            "govt": 0
-        }
-    },
-    {
-        "question": "Industrial pollution is an acceptable sacrifice for the sustainability of the modern world.",
-        "effect": {
-            "econ": 0,
-            "dipl": -20,
-            "govt": 0
-        }
-    },
-    {
-        "question": "Building floodwalls is the most optimal way to control and adapt to rising tides.",
-        "effect": {
-            "econ": 0,
-            "dipl": -20,
-            "govt": 0
-        }
-    },
-    {
-        "question": "Building dams or modifying land for human benefit is usually optimal.",
-        "effect": {
-            "econ": 0,
-            "dipl": -20,
-            "govt": 0
-        }
-    },
-    {
-        "question": "Creating large areas of crops and livestock that are resilient to adverse changes is more important than preserving biodiversity.",
-        "effect": {
-            "econ": 0,
-            "dipl": -20,
-            "govt": 0
-        }
-    },
-    {
-        "question": "An agrarian life is superior to an urban one.",
-        "effect": {
-            "econ": 0,
-            "dipl": 20,
-            "govt": 20
-        }
-    },
-    {
-        "question": "If artificial intelligence is to be developed, then its self-awareness should be curtailed.",
         "effect": {
             "econ": -5,
             "dipl": 0,
@@ -216,10 +129,98 @@ questions = [
         }
     },
     {
-        "question": "I try to recycle whenever possible.",
+        "question": "Under certain circumstances, it is acceptable to compromise on one's beliefs.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 10
+        }
+    },
+    {
+        "question": "The current human form is the ultimate form that a living being can have, and any modification deviates from that perfection.",
+        "effect": {
+            "econ": 20,
+            "dipl": 0,
+            "govt": -10
+        }
+    },
+    {
+        "question": "The needs of human settlers outweigh the importance of maintaining an environment's biodiversity.",
         "effect": {
             "econ": 0,
             "dipl": 20,
+            "govt": 0
+        }
+    },
+    {
+        "question": "The well-being of the Earth should be prioritized over space colonization.",
+        "effect": {
+            "econ": 0,
+            "dipl": -20,
+            "govt": 0
+        }
+    },
+    {
+        "question": "If an alien species is discovered, it should be respected and preserved regardless of its intelligence or attitude towards humanity.",
+        "effect": {
+            "econ": -5,
+            "dipl": -5,
+            "govt": 0
+        }
+    },
+    {
+        "question": "Industrial pollution is an acceptable sacrifice for the sustainability of the modern world.",
+        "effect": {
+            "econ": 0,
+            "dipl": 20,
+            "govt": 0
+        }
+    },
+    {
+        "question": "Building floodwalls is the most optimal way to control and adapt to rising tides.",
+        "effect": {
+            "econ": 0,
+            "dipl": 20,
+            "govt": 0
+        }
+    },
+    {
+        "question": "Building dams or modifying land for human benefit is usually optimal.",
+        "effect": {
+            "econ": 0,
+            "dipl": 20,
+            "govt": 0
+        }
+    },
+    {
+        "question": "Creating large areas of crops and livestock that are resilient to adverse changes is more important than preserving biodiversity.",
+        "effect": {
+            "econ": 0,
+            "dipl": 20,
+            "govt": 0
+        }
+    },
+    {
+        "question": "An agrarian life is superior to an urban one.",
+        "effect": {
+            "econ": 0,
+            "dipl": -20,
+            "govt": -20
+        }
+    },
+    {
+        "question": "If artificial intelligence is to be developed, then its self-awareness should be curtailed.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": -5
+        }
+    },
+    {
+        "question": "I try to recycle whenever possible.",
+        "effect": {
+            "econ": 0,
+            "dipl": -20,
             "govt": 0
         }
     },
@@ -227,7 +228,7 @@ questions = [
         "question": "Government regulations to protect the environment are justified.",
         "effect": {
             "econ": 0,
-            "dipl": 20,
+            "dipl": -20,
             "govt": 0
         }
     },
@@ -235,7 +236,7 @@ questions = [
         "question": "Climate change is the greatest threat to the well-being of modern society.",
         "effect": {
             "econ": 0,
-            "dipl": 20,
+            "dipl": -20,
             "govt": 0
         }
     },
@@ -244,7 +245,7 @@ questions = [
         "effect": {
             "econ": 0,
             "dipl": 0,
-            "govt": -20
+            "govt": 20
         }
     },
     {
@@ -252,23 +253,23 @@ questions = [
         "effect": {
             "econ": 0,
             "dipl": 0,
-            "govt": 20
+            "govt": -20
         }
     },
     {
         "question": "It is acceptable to curtail scientific progress so that cultural understanding and appreciation of certain technologies can catch up.",
         "effect": {
-            "econ": -5,
+            "econ": 10,
             "dipl": 0,
-            "govt": 20
+            "govt": -20
         }
     },
     {
         "question": "If there is a meaning to life, it is to live a moral life.",
         "effect": {
-            "econ": -5,
+            "econ": 10,
             "dipl": 0,
-            "govt": 20
+            "govt": -20
         }
     },
     {
@@ -276,7 +277,7 @@ questions = [
         "effect": {
             "econ": 0,
             "dipl": 0,
-            "govt": -20
+            "govt": 20
         }
     },
     {
@@ -284,7 +285,7 @@ questions = [
         "effect": {
             "econ": 0,
             "dipl": 0,
-            "govt": 20
+            "govt": -10
         }
     },
     {
@@ -292,7 +293,7 @@ questions = [
         "effect": {
             "econ": 0,
             "dipl": 0,
-            "govt": 20
+            "govt": -20
         }
     }
 ];
